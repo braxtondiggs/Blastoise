8306e2d500a676d422e3fc47d04291d29574964a
Braxton Diggs <braxtondiggs@gmail.com>
2020-02-13 15:13:46 -0500

    Create main.yml

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,12 @@
+import * as express from 'express';
+import * as cors from 'cors';
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+app.use(cors({ origin: true }));
+
+app.post('/', (req, res) => {
+    return res.status(200).json({ 'message': 'Ok' });
+});
+
+exports.endpoint = functions.https.onRequest(app);
