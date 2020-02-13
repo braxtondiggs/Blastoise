e4309ab25d8917f8ffb69180b199ca313d7b4cd7
Braxton Diggs <braxtondiggs@gmail.com>
2020-02-13 17:35:22 -0500

    Implement OpenBreweryDB

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,28 @@
-import * as express from 'express';
-import * as cors from 'cors';
+import express from 'express';
+import cors from 'cors';
+import axios, { AxiosResponse, AxiosError } from 'axios';
 import * as functions from 'firebase-functions';
 
 const app = express();
 app.use(cors({ origin: true }));
 
 app.post('/', (req, res) => {
-    return res.status(200).json({ 'message': 'Ok' });
+    const { address, name, type } = req.body;
+    if (address && name && type) {
+        return axios({
+            method: 'get',
+            url: 'https://api.openbrewerydb.org/breweries/search',
+            data: { query: name }
+        }).then((response: AxiosResponse) => {
+            if (response.data && response.data.length > 0) {
+                const data = response.data[0];
+
+                return res.status(200).send(data);
+            }
+            return res.status(200).json({});
+        }).catch((error: AxiosError) => res.status(400).json(error.toJSON()))
+    }
+    return res.status(400).send('Bad Request: An error has occured');
 });
 
 exports.endpoint = functions.https.onRequest(app);
